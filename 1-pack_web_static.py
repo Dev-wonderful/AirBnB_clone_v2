13f6e9d18339bee635cb26fb4f86e29166fb787e
Dev-wonderful <adeonederful20@gmail.com>
2023-07-09 01:00:43 +0100

    def

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -18,9 +18,8 @@ def do_pack():
     if result.return_code == 0:
         location = os.path.join(full_path, archived_file)
         size = os.path.getsize(location)
-        ret = 'web_static packed: versions/{} -> {}Bytes'
-               .format(archived_file, size)
-        return ret
+        print('web_static packed: versions/{} -> {}Bytes'.format(archived_file,
+                                                                 size))
     else:
         return None
     
