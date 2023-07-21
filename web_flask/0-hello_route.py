c7c97711b47e1a7e951e75176cc8423061af0bc1
Dev-wonderful <adeonderful20@gmail.com>
2023-07-21 23:49:03 +0100

    docstring and shebang

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+"""Hello world using flask framework"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """Default route"""
     return "Hello HBNB!"
