ade50fa4d39acf6a35994463b2e34c451f63ba89
Dev-wonderful <adeonderful20@gmail.com>
2023-07-23 00:08:23 +0100

    accepts only number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Hello world using flask framework"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Default route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """c route"""
+    text = text.replace('_', ' ')
+    return "C {}".format(escape(text))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is_cool"):
+    """python route"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(escape(text))
+
+
+@app.route("/number/<int:n>")
+def number_route(n):
+    """Number route"""
+    return "{} is a number".format(escape(n))
+
+
+if __name__ == '__main__':
+    app.run('localhost', debug=True)
