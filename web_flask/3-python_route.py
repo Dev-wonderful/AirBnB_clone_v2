6b14420bfb96a17730e8c93692d63b69ff2a5f21
Dev-wonderful <adeonderful20@gmail.com>
2023-07-22 23:44:02 +0100

    optional route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Hello world using flask framework"""
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -21,14 +22,15 @@ def hbnb():
 def c_route(text):
     """c route"""
     text = text.replace('_', ' ')
-    return "C {}".format(text)
+    return "C {}".format(escape(text))
 
 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python_route(text="is cool"):
+def python_route(text="is_cool"):
     """python route"""
     text = text.replace('_', ' ')
-    return "Python {}".format(text)
+    return "Python {}".format(escape(text))
 
 
 if __name__ == '__main__':
